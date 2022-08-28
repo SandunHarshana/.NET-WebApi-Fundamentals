1dfde4b5701ef39934110676685fafdea99c7d00
SandunHarshana <wksharshana@gmail.com>
2022-08-28 23:24:50 +1000

    Created api functions to create, update and delete

--- Controllers/ItemsController.cs
+++ Controllers/ItemsController.cs
@@ -24,7 +24,7 @@ namespace Catalog.Controller
         }
 
         [HttpGet("{id}")]
-        public ActionResult<ItemDto> getItems(Guid id)
+        public ActionResult<ItemDto> GetItem(Guid id)
         {
             var item = repository.GetItem(id);
             if(item is null)
@@ -32,6 +32,52 @@ namespace Catalog.Controller
                 return NotFound();
             }
             return item.AsDto();
+        }
+
+        [HttpPost]
+        public ActionResult<ItemDto> CreateItem(CreateItemDto itemDto)
+        {
+            Item item = new()
+            {
+                Id = Guid.NewGuid(),
+                Name = itemDto.Name,
+                Price = itemDto.Price,
+                CreatedDate = DateTimeOffset.UtcNow
+            };
+            repository.CreateItem(item);
+
+            return CreatedAtAction(nameof(GetItem), new{ id = item.Id}, item.AsDto());
+        }
+
+        [HttpPut("{id}")]
+        public ActionResult UpdateItem(Guid id, UpdateItemDto itemDto)
+        {
+            var existingItem = repository.GetItem(id);
+            if (existingItem is null)
+            {
+                return NotFound();
+            }
+            Item updatedItem = existingItem with
+            {
+                Name = itemDto.Name,
+                Price = itemDto.Price
+            };
+            repository.UpdateItem(updatedItem);
+            return NoContent();
+        }
+
+
+        [HttpDelete("{id}")]
+        public ActionResult DeleteItem(Guid id)
+        {
+            var existingItem = repository.GetItem(id);
+            if (existingItem is null)
+            {
+                return NotFound();
+            }
+
+            repository.DeleteItem(id);
+            return NoContent();
         }
     }
 }
--- Repositories/IItemsRepository.cs
+++ Repositories/IItemsRepository.cs
@@ -6,7 +6,9 @@ namespace Catalog.Repository
     public interface IItemsRepository
     {
         Item GetItem(Guid id);
-
         IEnumerable<Item> GetItems();
+        void CreateItem(Item item);
+        void UpdateItem(Item item);
+        void DeleteItem(Guid id);
     }
 }
